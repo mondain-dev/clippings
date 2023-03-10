af3d4c0a35ec497965bbbe2914ad6a403a923370
mondain-dev <mondain.sciamhaevensis@gmail.com>
2023-03-10 16:24:57 +1100

    update

--- routes/clipping.js
+++ routes/clipping.js
@@ -5,10 +5,17 @@ import fs from 'fs'
 
 import {unified} from 'unified'
 import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import remarkFrontmatter from 'remark-frontmatter'
+import extract  from "remark-extract-frontmatter"
+import frontmatter from 'remark-frontmatter'
+import remarkMath from 'remark-math'
 import remarkGfm from 'remark-gfm'
+import remarkRehype from 'remark-rehype'
+import rehypeRaw from "rehype-raw"
+import rehypeMeta from "rehype-meta"
+import rehypeDocument  from "rehype-document"
 import rehypeStringify from 'rehype-stringify'
+import rehypeKatex from 'rehype-katex'
+import yaml from "yaml"
 
 import config from "../config.js"
 
@@ -19,15 +26,30 @@ router.get("/*", (req, res) => {
     }
     else if(clippingFile.toLowerCase().endsWith('.md')){
         fs.readFile(clippingFile, 'utf8', (err, data) => {
-            unified()
-            .use(remarkParse)
-            .use(remarkFrontmatter)
-            .use(remarkGfm)
-            .use(remarkRehype)
-            .use(rehypeStringify)
-            .process(data, (err, file) => {
-                res.send("<!DOCTYPE html><html><head> <link rel=\"stylesheet\" href=\"..\/css\/foghorn.css\"><\/head><body>" + String(file) + "<\/body><\/html>")
+           unified()
+           .use(remarkParse)
+           .use(remarkMath)
+           .use(extract, { yaml: yaml.parse, name: 'meta' })
+           .use(frontmatter)
+        //    .use(() => (data) => {
+        //         data.children.filter( (e) => e.type=="yaml").forEach( e => {
+        //             e.type = "code"
+        //             e.lang = "yaml"
+        //         });
+        //         console.dir(data);
+        //     })
+           .use(remarkGfm)
+           .use(remarkRehype, {"allowDangerousHtml": true})
+           .use(rehypeRaw)
+           .use(rehypeKatex)
+           .use(rehypeDocument, {
+                css: ['../css/foghorn.css', 'https://cdn.jsdelivr.net/npm/katex@0.15.0/dist/katex.min.css']
             })
+           .use(rehypeMeta)
+           .use(rehypeStringify)
+           .process(data, (err, file) => {
+               res.send(String(file))
+           })
         })
     }
     else if(clippingFile.toLowerCase().endsWith('.pdf')) {
